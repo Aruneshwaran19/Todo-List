4e8a7d7b4c87a8f821f605cac93322c79de314ee
Aruneshwaran <aruneshwaran.v19@gmail.com>
2026-05-18 15:49:17 +0530

    Configure CORS with dynamic frontend URL

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("API is running!"));
